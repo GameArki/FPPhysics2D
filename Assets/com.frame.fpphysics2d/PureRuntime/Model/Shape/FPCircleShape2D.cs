2b9dcdba9315d1c7641fe00f991f856ce5632848
onovich <hjn1110@163.com>
2022-11-13 08:19:10 +0800

    <fix> modify: CircleShape 的 PruneBounding Size 由半径改为直径

--- Assets/com.frame.fpphysics2d/PureRuntime/Model/Shape/FPCircleShape2D.cs
+++ Assets/com.frame.fpphysics2d/PureRuntime/Model/Shape/FPCircleShape2D.cs
@@ -25,7 +25,7 @@ namespace JackFrame.FPPhysics2D {
         }
 
         FPBounds2 IShape2D.GetPruneBounding(FPTransform2D tf) {
-            FPVector2 tarSize = new FPVector2(radius, radius);
+            FPVector2 tarSize = new FPVector2(radius * 2, radius * 2);
             return new FPBounds2(tf.Pos, tarSize);
         }
 
