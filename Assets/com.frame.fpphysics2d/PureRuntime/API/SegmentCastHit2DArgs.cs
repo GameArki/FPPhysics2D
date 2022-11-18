ffc513c3aea491cce7c7239b91601bb4f2a390c8
onovich <hjn1110@163.com>
2022-11-18 23:47:37 +0800

    <refactor> add: FPSegment2D / FPRay2D

---
+++ Assets/com.frame.fpphysics2d/PureRuntime/API/SegmentCastHit2DArgs.cs
@@ -0,0 +1,12 @@
+using FixMath.NET;
+
+namespace JackFrame.FPPhysics2D.API {
+
+    public struct SegmentCastHit2DArgs {
+
+        public FPRigidbody2DEntity rigidbody;
+        public FPVector2[] points;
+
+    }
+
+}
--- Assets/com.frame.fpphysics2d/Sample/FPPhysics2DSampleApp.cs
+++ Assets/com.frame.fpphysics2d/Sample/FPPhysics2DSampleApp.cs
@@ -30,7 +30,7 @@ namespace JackFrame.Sample {
 
             allGo = new GameObject[6];
             allRB = new FPRigidbody2DEntity[6];
-            /*
+            
             for (int i = 0; i < allGo.Length; i += 1) {
                 allGo[i] = new GameObject("go" + i.ToString());
             }
@@ -53,6 +53,7 @@ namespace JackFrame.Sample {
                 }
             }
 
+            /*
             role.OnTriggerEnterHandle += (ev) => {
                 System.Console.WriteLine("Trigger Enter");
             };
@@ -89,12 +90,13 @@ namespace JackFrame.Sample {
                 stay2 = false;
             };
             */
+            
 
         }
 
         // Update is called once per frame
         void Update() {
-            /*
+            
             var velo = role.LinearVelocity;
             velo.y -= 1;
             if (Input.GetKeyDown(KeyCode.Space)) {
@@ -112,7 +114,7 @@ namespace JackFrame.Sample {
             role.SetLinearVelocity(velo);
 
             space2D.Tick(16 * FP64.EN3);
-            */
+            
         }
 
         void OnDrawGizmos() {
